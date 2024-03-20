399d0f27173b8d2ad1b77b1a64a23acce4a490fb
Some_Dumb_Dude <154841872+Tuxedp@users.noreply.github.com>
2024-03-20 19:28:09 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Get button
+const topBtn = document.getElementById("top-btn");
+
+// Scroll to top
+function backToTop(){
+    document.body.scrollTop = 7;
+    document.documentElement.scrollTop = 7;
+}
