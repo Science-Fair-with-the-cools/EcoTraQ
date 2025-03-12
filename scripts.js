b98df08845540cf01a2305b1e2e4501b382fa326
Some_Dumb_Dude <154841872+Tuxedp@users.noreply.github.com>
2025-03-12 19:37:28 -0400

    Only the essential stuff now

--- scripts.js
+++ scripts.js
@@ -8,87 +8,9 @@
     document.documentElement.scrollTop = 0;
   }
 
-  // Get select elements
-  const select_element1 = document.getElementById("select-element1");
-  const select_element2 = document.getElementById("select-element2");
-  const select_element3 = document.getElementById("select-element3");
-  const select_element4 = document.getElementById("select-element4");
-  const select_element5 = document.getElementById("select-element5");
-  const select_element6 = document.getElementById("select-element6");
-  const select_element7 = document.getElementById("select-element7");
-  const select_element8 = document.getElementById("select-element8");
-  const select_element9 = document.getElementById("select-element9");
-  const select_element10 = document.getElementById("select-element10");
-
-// Define emission values for each option
-const emissions = {
-    transportation: {
-      "Car": 3.3863,
-      "EV": 0,
-      "Public_Bus": 1.25125,
-      "Bike": 0,
-      "Walking": 0,
-      //q2
-      "multiple_times": 1.008,
-      "once": 0.336,
-      "rarely": 0,
-    },
-    housing: {
-      "town": 6.08,
-      "single": 13.1833333333,
-      "apartment": 10.33,
-      "mobile": 1.14,
-      //q
-      "Gas": 3.265,
-      "Elec": 3.311,
-      //q
-      "None": 0,
-      "One": 0.1325,
-      "Two": 0.265,
-      "Three": 0.3975,
-      "All": 0.53,
-    },
-    diet: {
-      "vegan": 1.5,
-      "vegetarian": 1.7,
-      "omnivore": 2.5,
-      "carnivore": 3.3,
-      "nobeef": 1.9,
-      //q
-      "several_times": 0.52811735,
-      "few_times": 0.41788635,
-      "rarely_never": 0.110231,
-    },
-      habits: {
-      "always0": 3.63,
-      "sometimes0": 1.8143694,
-      "rarely0": 0.9071847,
-        //q
-      "always2": -0.15,
-      "sometimes2": -0.10,
-      "rarely2": -0.05,
-      "idk_dude": 0,
-        //q
-      "always3": 0.0346,
-      "sometimes3": 0.0174,
-      "rarely3": 0.0110231,
-      "idk4": 0.0087,
-    }
-  };
-
   // Function to calculate total emissions for a section
 function calculateSectionEmissions(sectionName) {
     let sectionTotal = 0;
-    let sectionArray = emissions[sectionName];
-
-    for(var key in sectionArray){
-        // if the array the value corresponds to the current selection {
-            if(key == select_element1.value || key == select_element2.value || key == select_element3.value || key == select_element4.value || key == select_element5.value || key == select_element6.value || key == select_element7.value || key == select_element8.value || key == select_element9.value || key == select_element10.value){
-                sectionTotal += sectionArray[key];
-            }
-        }
-    
-    return sectionTotal;
 }
 
     const numInput = document.getElementById("numInput");
@@ -99,9 +21,9 @@ function calculateSectionEmissions(sectionName) {
     // Get the div so we can make the buttons
     const summaryBtns = document.getElementById("nav-btns");
 
-    const graph = document.getElementById("chart");
+    const piGraph = document.getElementById("chart");
 
-document.getElementById("goof").addEventListener("click", function CalcEmissions (event) {
+document.getElementById("sumbitButton").addEventListener("click", function CalcEmissions (event) {
         event.preventDefault();
 
         if(select_element1.value == "" || select_element2.value == "" || select_element3.value == "" || select_element4.value == "" || select_element5.value == "" || select_element6.value == "" || numInput.value.toString() == "" || select_element8.value == "" || select_element9.value == "" || select_element10.value == "") {
@@ -121,7 +43,7 @@ document.getElementById("goof").addEventListener("click", function CalcEmissions
 
         // Display result
         let resultElement = document.getElementById("result");
-        resultElement.innerText = `Your estimated total carbon footprint is ${totalEmissions} tons of CO2 per year. \nYour estimated carbon footprint was ${numInput.value.toString()} tons. \nTake a look at how they compare! \n**For Reference** Ontario’s emissions per capita are the third lowest in Canada, at 10.1 tonnes of CO2 \nThis is 43% below the Canadian average of 17.7 tonnes per capita.`;
+        // Change what this says: resultElement.innerText = `Your estimated total carbon footprint is ${totalEmissions} tons of CO2 per year. \nYour estimated carbon footprint was ${numInput.value.toString()} tons. \nTake a look at how they compare! \n**For Reference** Ontario’s emissions per capita are the third lowest in Canada, at 10.1 tonnes of CO2 \nThis is 43% below the Canadian average of 17.7 tonnes per capita.`;
 
           // Create buttons
           summaryBtns.innerHTML = `<button id="btn1" onclick="summaryManager(1)"></button>
